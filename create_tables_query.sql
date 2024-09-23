4fc17c7b1a4f48a9542f061c2a67d7280e3a16c6
pascalypperciel <p_ypperc@live.concordia.ca>
2024-09-23 14:28:45 -0400

    Modified query from MySQL to SQL

--- create_tables_query.sql
+++ create_tables_query.sql
@@ -5,27 +5,27 @@ CREATE TABLE Students (
 );
 
 CREATE TABLE Teachers (
-    TeacherID INT PRIMARY KEY AUTO_INCREMENT,
+    TeacherID INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(100) NOT NULL,
     Password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Courses (
-    CourseID INT PRIMARY KEY AUTO_INCREMENT,
+    CourseID INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(100) NOT NULL,
     TeacherID INT,
     FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID)
 );
 
 CREATE TABLE Groups (
-    GroupID INT PRIMARY KEY AUTO_INCREMENT,
+    GroupID INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(100) NOT NULL,
     CourseID INT,
     FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
 );
 
 CREATE TABLE Ratings (
-    RatingID INT PRIMARY KEY AUTO_INCREMENT,
+    RatingID INT PRIMARY KEY IDENTITY(1,1),
     CooperationRating INT NOT NULL,
     ConceptualContributionRating INT NOT NULL,
     PracticalContributionRating INT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE Ratings (
 );
 
 CREATE TABLE StudentGroup (
-    StudentGroupID INT PRIMARY KEY AUTO_INCREMENT,
+    StudentGroupID INT PRIMARY KEY IDENTITY(1,1),
     StudentID INT,
     GroupID INT,
     FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
