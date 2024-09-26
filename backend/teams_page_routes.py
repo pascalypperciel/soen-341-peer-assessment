84be06fffec10d9045127383888502dd7f4ed5ab
Anthony <antoguarr@gmail.com>
2024-09-26 12:41:27 -0400

    Update teams_page_routes.py
    added documentation for csv team creation route, started manual add route

--- backend/teams_page_routes.py
+++ backend/teams_page_routes.py
@@ -8,10 +8,33 @@ import csv
 
 teams_page_routes = Blueprint('teams_page_routes', __name__)
 
+#Method for instructor to manually create a team given specific fields 
+# -> student ID manual input , group id assessed based on current inputs in db
 
-@teams_page_routes.route('/maketeam',methods=['POST'])
-def make_team():
+
+@teams_page_routes.route('maketeamsManually', methods= ['POST'])
+def make_team_manually():
+     # Get the list of student IDs from the form submission (e.g., a comma-separated string or array)
+    student_ids = request.form.getlist('student_ids')  # Assumes a form input named 'student_ids'
+    
+    #if no students return error 
+    if not student_ids:
+        return jsonify({"error": "No student IDs provided"}), 400
+
+    
+
+
+
+    
+
+
+#Method for instructor to create a team with a csv upload 
+# csv format is assumed to be studentID in each row, when there is an empty row, signaling of end of that specific team 
+@teams_page_routes.route('/maketeamCSV',methods=['POST'])
+def make_team_CSV():
+    #get the file from the front end 
     file = request.files['file']
+    #error handle if the file is not there
     if not file:
         return jsonify({"error": "No file provided"}), 400
 
@@ -25,7 +48,7 @@ def make_team():
 
     # read and parse the CSV file
     for row in csv_reader:
-        if not row:  #
+        if not row:  #if empty line, means end of team 
             if current_team:
                 teams.append((current_team, group_id))
                 current_team = []
@@ -40,11 +63,12 @@ def make_team():
     if current_team:  # Add the last group if not already added
         teams.append((current_team, group_id))
 
-    #add correct cursor connection for future use/ implementation
+    #add correct cursor connection for the live server - > this set up assumes local hosting.
     cursor = conn.cursor()
     try:
         for team, group_id in teams:
             for student_id in team:
+                #query to put the teams into the db
                 query = """
                 INSERT INTO StudentGroup (StudentID, GroupID)
                 VALUES (%s, %s)
