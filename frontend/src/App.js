8725d488c49a2bee4a6382d350a04e108c1b0565
Jessicuta <jessicacodreanu@gmail.com>
2024-09-27 20:51:34 -0400

    fix the display depending of the screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SignupPage from './Components/Sign-Login/SignupPage'; 
-import HomeStudent from './Components/Sign-Login/homeStudent'; 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import SignupPage from "./Components/Sign-Login/SignupPage";
+import HomeStudent from "./Components/Sign-Login/homeStudent";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Redirect from "/" to "/signup" */}
         <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/homeStudent" element={<HomeStudent />} />
