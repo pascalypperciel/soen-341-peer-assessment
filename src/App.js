3a943b01e09d623ca15d21ac98b7cb583c62f570
Extinctable <135893520+Extinctable@users.noreply.github.com>
2024-09-30 19:20:16 -0400

    Add files via upload
    An interactive sidebar template using react for the project. Will modify with correct elements.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Reports from "./pages/Reports";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={Home} />
+          <Route path="/products" element={Products} />
+          <Route path="/reports" element={Reports} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
