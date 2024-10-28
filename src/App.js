c2c61c020068254e922eda27d3a5991362791ed8
Extinctable <135893520+Extinctable@users.noreply.github.com>
2024-10-27 20:06:29 -0400

    Updated header component
    This updated header component adds more functionality to the website and implements a better UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import "./styles/styles.scss";
+import navigation from "./data";
+import { ReactComponent as Logo } from "./assets/concordia-university-1.svg";
+
 import Header from "./components/Header";
 
 export default function App() {
-  return <Header />;
+  return (
+    <div className="App">
+      <Header items={navigation} logo={<Logo />} navPosition="center" />
+      <section className="hero">
+        <img src="Concordia-background.jpg" alt="Extinctable" />
+      </section>
+    </div>
+  );
 }
