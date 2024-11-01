39e4e9459ae45411fe3d6d48815cbf31a50e39d0
pascalypperciel <pypperciel@gmail.com>
2024-10-31 20:42:01 -0400

    Revert "Parse the db connection string when reusing user-story-0.yml"
    This reverts commit 68915c15c04d37b513f313966433bfb5a2c561ba.

--- backend/db.py
+++ backend/db.py
@@ -9,15 +9,6 @@ username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
 driver = os.getenv('DB_DRIVER')
 
-print("Server:", server)
-print("Database:", database)
-print("Username:", username)
-print("Password:", password)
-print("Driver:", driver)
-
-if None in [server, database, username, password, driver]:
-    raise ValueError("One or more required environment variables are missing")
-
 conn = pyodbc.connect(
     'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password
 )
