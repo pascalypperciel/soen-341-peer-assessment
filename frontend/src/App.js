5dfaa3e6d9258e42b6f3f3b7645b2653aa2dc187
Extinctable <massimo02caruso@gmail.com>
2024-11-10 22:50:08 -0500

    Contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 
 import Landing from "./Components/landingPage/Landing";
+import Contact from "./Components/contact/Components/Contact";
 import SignupPage from "./Components/Sign-Login/SignupPage";
 import Teams from "./Components/TeamPage/Teams";
 import RatingPage from "./Components/ratingPage/ratingpage";
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/" element={<Navigate to="/landing" />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/ratingpage" element={<RatingPage />} />
         <Route path="/shortsummary" element={<ShortSummary />} />
